e80e5025e713afe32cea4663b8abe55ea7d2f951
Paul <oliibert@hotmail.com>
2018-10-08 22:41:27 +0300

    uuendus

--- views/ois/courses.py
+++ views/ois/courses.py
@@ -5,11 +5,11 @@ def coursesId(id):
     status_code = request.status_code
     if status_code == 200:
         return request.json()
-    raise Exception("reguest status code: " + status_code)
+    raise Exception("reguest status code: ",status_code)
 
 def getNCourses(n: int, start: int):
     request = requests.get('https://ois2.ut.ee/api/courses?take=' + str(n) + '&start=' + str(start))
     status_code = request.status_code
     if status_code == 200:
         return request.json()
-    raise Exception("reguest status code: " + str(status_code))
+    raise Exception("reguest status code: ",status_code)
