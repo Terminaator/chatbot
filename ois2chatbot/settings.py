1643bec08cee4ba6b8d1a7b779b959bb9247ad42
Paul <oliibert@hotmail.com>
2018-10-10 00:44:59 +0300

    uuendus

--- ois2chatbot/settings.py
+++ ois2chatbot/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'requests',
 ]
 
 MIDDLEWARE = [
@@ -97,6 +96,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
