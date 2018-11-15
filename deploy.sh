0e366d530288f31f881bdd73cdf4bcb725655873
MeelisP <meelis.perli@gmail.com>
2018-11-15 14:10:09 +0200

    still fixing travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 tvp2017-dumas.pem # Allow read access to the private key
+chmod 700 tvp2017-dumas.pem # Allow read access to the private key
 ssh-add tvp2017-dumas.pem # Add the private key to SSH
 
 git config --global push.default matching
-git remote add deploy ssh://git@193.40.33.98:22/var/www/chatbot2/chatbot
-git push deploy master
+git remote add deploy ubuntu@193.40.33.98/var/www/chatbot2/chatbot.git
+git push deploy
 
 ssh apps@193.40.33.98 -p 22 <<EOF
   cd /var/www/chatbot2/chatbot
