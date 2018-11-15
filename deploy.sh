55812079747288c9ef49ce34893db958b1dafb50
MeelisP <meelis.perli@gmail.com>
2018-11-15 13:00:20 +0200

    still fixing travis.yml

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ chmod 600 tvp2017-dumas.pem # Allow read access to the private key
 ssh-add tvp2017-dumas.pem # Add the private key to SSH
 
 git config --global push.default matching
-#git remote add deploy ssh://git@193.40.33.98:22/var/www/chatbot2/chatbot
+git remote add deploy ssh://git@193.40.33.98:22/var/www/chatbot2/chatbot
 git push deploy master
 
 ssh apps@193.40.33.98 -p 22 <<EOF
