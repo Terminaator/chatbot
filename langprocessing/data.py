9f5637044bc7a0ecfcc060162c8dc55b0065bac6
Paul <oliibert@hotmail.com>
2018-12-05 17:58:44 +0200

    oh boy, my cookie went bad

--- langprocessing/data.py
+++ langprocessing/data.py
@@ -1,8 +1,10 @@
 class Data:
     def __init__(self,askedQuestion,frames,currentFrame):
+        #chatbot
         self.askedQuestion = askedQuestion
         self.frames = frames
         self.currentFrame = currentFrame
+        #hangman
 
     def __str__(self):
         return str(self.askedQuestion) + " " + str(self.frames) + " " + str(self.currentFrame)
--- oisbotServer/views/client/clientQuestionView.py
+++ oisbotServer/views/client/clientQuestionView.py
@@ -7,7 +7,7 @@ from oisbotServer.views.json.requestJson import RequestJson
 from django.core.cache import cache
 from langprocessing.data import Data
 from secrets import token_urlsafe
-
+time = 60*10
 @method_decorator(csrf_exempt, name='dispatch') #Võimalik, et me eemaldame selle ära
 class ClientQuestionView(View):
     def __init__(self, **kwargs):
@@ -21,13 +21,13 @@ class ClientQuestionView(View):
         cookie = request.COOKIES.get("TakeMeHome")
         if self.getCookie(cookie):
             response = JsonResponse(self.bot.getResponse(inputSentence))
-            cache.set(cookie,self.bot.getData())
+            cache.set(cookie,self.bot.getData(),time)
             return response
         else:
             key = token_urlsafe(16)
             response = JsonResponse(self.bot.getResponse(inputSentence))
-            cache.set(key,self.bot.getData())
-            response.set_cookie("TakeMeHome",key)
+            cache.set(key,self.bot.getData(),time)
+            response.set_cookie("TakeMeHome",key,max_age=time)
             return response
 
 
@@ -35,6 +35,6 @@ class ClientQuestionView(View):
         if cookie is None:
             return False
         else:
-            data = cache.get_or_set(cookie, Data(self.bot.askedQuestion,self.bot.frames,self.bot.currentFrame))
+            data = cache.get_or_set(cookie, Data(self.bot.askedQuestion,self.bot.frames,self.bot.currentFrame),time)
             self.bot.setData(data)
             return True
